b564e1437773317e96f807e05154b75d0156bce7
Sigma53Tr <hamzadilmac61@gmail.com>
2026-02-02 23:48:25 +0300

    Create hop.lua

---
+++ hop.lua
@@ -0,0 +1,7 @@
+_G.room = "HamzaVicsSistemi"
+_G.mainuser = "hamza07e"
+_G.searcher = true
+_G.mainwait = true
+
+-- Bu satır senin az önce oluşturduğun logic.lua dosyasını çalıştıracak
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sigma53Tr/victus-script/main/logic.lua"))()
